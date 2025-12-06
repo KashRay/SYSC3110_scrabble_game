aff6d49cc40005fad8853309698b7ce654b73f84
Abdullah Khan <abkhan0517@gmail.com>
2025-12-05 23:14:25 -0500

    Fixed serialization issue

--- src/PlayerMove.java
+++ src/PlayerMove.java
@@ -1,4 +1,6 @@
-public record PlayerMove(int totalScore, String mainWord) {
+import java.io.Serializable;
+
+public record PlayerMove(int totalScore, String mainWord) implements Serializable{
     @Override
     public String toString() {
         return mainWord + " (" + totalScore + " pts)";
